05a6381f28e8f3a3618d92f13129f702a34b2b57
Michal <mkopij000@gmail.com>
2021-03-14 13:17:43 +0100

    Utworzono klasy SiecNeuronowa, PrzetwarzaczObrazow,

---
+++ 6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/PrzetwarzaczObrazow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+
+namespace _6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów
+{
+    // klasa odpowiada za przygotowanie obrazow do przetworzenia przez siec neuronowa
+    public static class PrzetwarzaczObrazow
+    {
+       
+    }
+}
---
+++ 6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/SiecNeuronowa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów
+{
+    class SiecNeuronowa
+    {
+    }
+}
