4c1ca21e5a1ca36257a8a0930faa9e1f8e847dd6
Michal <mkopij000@gmail.com>
2021-03-14 13:36:53 +0100

    Dodano funkcje kompresujListeObrazow do klasy PrzetwarzaczObrazow

--- 6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/PrzetwarzaczObrazow.cs
+++ 6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów/PrzetwarzaczObrazow.cs
@@ -14,11 +14,22 @@ namespace _6938_sieci_neuronowe_lab1_rozpoznawanie_pojazdów
     public static class PrzetwarzaczObrazow
     {
         // funkcja zmniejsza rozmiar obrazu, i wyszarza go, aby zaoszczedzic na miejscu i pamieci
-        public static Image<Gray, Byte> kompresujObraz(Image<Bgr, Byte> oryginalnyObraz)
+        public static Image<Gray, Byte> kompresujObraz(Image<Gray, Byte> oryginalnyObraz)
         {
             var wyjsciowyObraz = oryginalnyObraz.Convert<Gray, Byte>(); // Wyszarza obraz
 
             return wyjsciowyObraz.Resize(50, 50, Emgu.CV.CvEnum.Inter.Cubic, false); // Zmiejsza obraz i go zwraca
         }
+
+        // Kompresuje cala liste obrazow
+        public static List<Image<Gray, Byte>> kompresujListeObrazow(List<Image<Gray, Byte>> listaWejsciowa)
+        {
+            List<Image<Gray, Byte>> listaWyjsciowa = new List<Image<Gray, byte>>();
+
+            listaWejsciowa.ForEach(o => listaWyjsciowa.Add(kompresujObraz(o)));
+
+            return null;
+        }
+
     }
 }
